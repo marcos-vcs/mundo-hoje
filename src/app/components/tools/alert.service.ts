a8b057b279ac9d603bda746fa661f80160140b49
marcos-vcs <marcosviniciocardoso123@gmail.com>
2022-09-15 23:43:00 -0300

    improvement: service alert and service toast

---
+++ src/app/components/tools/alert.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+ //#region variables
+  private handlerMessage = false;
+ //#endregion
+
+  constructor(private alertController: AlertController) { }
+
+  //#region methods
+  async presentAlert(title: string, description: string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: title,
+      message: description,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async presentConfirmAlert(title: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = false;
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = true;
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+    await alert.onDidDismiss();
+    return this.handlerMessage;
+  }
+  //#endregion
+
+}
---
+++ src/app/components/tools/toast.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(private toastController: ToastController) { }
+
+  async presentToast(text: string, positionOnScreen: 'top' | 'middle' | 'bottom',
+  toastColor: 'danger' | 'success' | 'warning' | 'tertiary' | 'primary' | 'medium' | 'secondary' | 'dark' | 'light') {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: 1500,
+      position: positionOnScreen,
+      color: toastColor
+    });
+    await toast.present();
+  }
+
+}
