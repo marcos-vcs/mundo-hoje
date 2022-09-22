daa260509a33880a4c52b91be2fb49a69e9ab516
marcos-vcs <marcosviniciocardoso123@gmail.com>
2022-09-22 17:24:15 -0300

    improvement: add refresh function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { IbgeNoticeApiService } from './services/ibge-notice-api.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +14,10 @@ import { HttpClientModule } from '@angular/common/http';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    IbgeNoticeApiService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
