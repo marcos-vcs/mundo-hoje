d0f489dd05ffd2198ff6829fec772cb83b1bc2ad
marcos-vcs <marcosviniciocardoso123@gmail.com>
2022-12-21 17:14:07 -0300

    text-align configuration in about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Configuration } from 'src/app/models/configuration';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-about',
@@ -8,9 +10,22 @@ import { ModalController } from '@ionic/angular';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  configuration: Configuration;
 
-  ngOnInit() {}
+  constructor(private modalCtrl: ModalController, private storage: StorageService) { }
+
+  ngOnInit() {
+    this.storage.init();
+    this.loadConfiguration();
+  }
+
+  async loadConfiguration(){
+    await this.storage.openStore();
+    const configuration = await (
+      await this.storage.getItem('configurations')
+    ).toString();
+    this.configuration = configuration ? JSON.parse(configuration) as Configuration : new Configuration();
+  }
 
   cancel() {
     return this.modalCtrl.dismiss('cancel');
